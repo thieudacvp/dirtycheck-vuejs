1c608f1f71a300ac52f4f3b7eda33fc2d4241b64
HieuNguyen <hieunp@newwave.vn>
2021-12-10 17:58:28 +0700

    demo dirtycheck

---
+++ demo-dirtycheck/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div>
+        <form>
+            <label for="fname">First name:</label><br />
+            <input type="text" id="fname" name="fname" v-model="firstname" /><br />
+            <label for="lname">Last name:</label><br />
+            <input type="text" id="lname" name="lname" v-model="lastname" />
+        </form>
+    </div>
+    <!-- <div>
+            <form-1/>
+    </div> -->
+  </div>
+</template>
+
+<script>
+//import form1 from './components/Form.vue'
+export default {
+  name: "App",
+  components: {
+//      form1
+  },
+
+  data() {
+    return {
+      firstname: null,
+      lastname: null,
+      dirtycheck: false,
+    };
+  },
+  methods: {},
+  
+  updated() {
+      window.onbeforeunload = function (event) {
+        var message =
+          "Important: Please click on 'Save' button to leave this page.";           
+        if (typeof event == "undefined") {
+          event = window.event;
+        }
+        if (event) {
+          event.returnValue = message;
+        }
+        return message;
+      };
+  },
+};
+</script>
+
+
---
+++ demo-dirtycheck/src/components/Form.vue
@@ -0,0 +1,28 @@
+<template>
+    <form>
+        <input v-model="form.first_name"/>
+        <input v-model="form.last_name"/>
+        <input v-model="form.email"/>
+    </form> 
+</template>
+
+<script>
+    const defaultForm = {
+        first_name: '',
+        last_name: '',
+        email: '',
+    }  
+    export default {
+      data () {
+        return {
+            form: defaultForm
+        }
+      },
+      computed: {
+        hasChanged () {
+          return Object.keys(this.form).some(field => this.form[field] !== defaultForm[field]),
+          console.log("changed")
+        }
+      }
+    }
+</script>
